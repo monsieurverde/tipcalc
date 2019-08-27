d7d6e87d41031bfb39b26c04a4768c315fc88347
monsieurverde <domverdemgmt@gmail.com>
2019-08-26 23:38:42 -0400

    tip calculator

---
+++ tipcalc.py
@@ -0,0 +1,15 @@
+#prompt the user for meal subtotal
+subtotal = input("Meal subtotal: ")
+subtotal = float(subtotal)
+
+# prompt the user for tip %
+tip_pct = input("Tip %: ")
+
+# convert % value to decimal value
+tip_pct = float(tip_pct) * 0.01
+
+#calculate the tip amount
+tip = subtotal * tip_pct
+
+# print tip amount
+print("Tip:",tip)
